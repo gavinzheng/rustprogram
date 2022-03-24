d3b399424d64bae93a332c3299962c6973afab11
gavindt <gavinzheng731@gmail.com>
2022-03-25 04:08:45 +0800

    init

---
+++ Data Type/structmutability.rs
@@ -0,0 +1,17 @@
+struct S1 { 
+	field1: int, 
+	field2: S2 
+}
+struct S2 { 
+	field: int 
+}
+fn main() { 
+	let s = S1 { field1: 45, field2: S2 { field: 23 } }; 
+	// s 是深度不可变的，所以下面的可变赋值是会出错的
+	// s.field1 = 46; 
+	// s.field2.field = 24; 
+	let mut s = S1 { field1: 45, field2: S2 { field: 23 } }; 
+	// s本身是可变的，所以对其域成员的复制是OK的 
+	s.field1 = 46; 
+	s.field2.field = 24; 
+} 
