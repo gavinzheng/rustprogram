8f29894cb56c3121232f8cee70ca32ed719c592e
gavindt <gavinzheng731@gmail.com>
2023-05-31 00:43:51 +0800

    20230530007

---
+++ sourcecode/chapter25/SolanaDapp-AnchorRust-React/tests/postfeedapp.js
@@ -0,0 +1,30 @@
+const anchor = require('@project-serum/anchor');
+const { assert } = require('chai');
+//const { assert } = require('assert');
+const {SystemProgram} = anchor.web3
+
+describe('postfeedapp', () => {
+  const provider = anchor.Provider.local();
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+  const program = anchor.workspace.Postfeedapp;
+  const feedPostApp = anchor.web3.Keypair.generate();
+  it('Is initialized!', async () => {
+    const num = new anchor.BN(2)
+    // Add your test here.
+    await program.rpc.createPost('hello','www.imagrurl.com',num,false,{
+      accounts:{
+        feedPostApp:feedPostApp.publicKey,
+        user:provider.wallet.publicKey,
+        systemProgram:SystemProgram.programId
+      },
+     signers:[feedPostApp]
+    })
+    const account = await program.account.feedPostApp.fetch(feedPostApp.publicKey) //get the accounts info from this fetch
+    //const tx = await program.rpc.initialize(); 
+    //console.log("Your transaction signature", tx);
+    assert.ok(account.media === 'www.imagrurl.com');
+    assert.ok(account.admin===false);
+    assert.ok(account.text==='hello');
+  });
+});
